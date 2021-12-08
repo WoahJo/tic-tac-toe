b0a0f1d06da54e4e96efabd74632854b0e8f7ef2
WoahJo <ccnettacc@gmail.com>
2021-12-08 18:34:49 -0500

    changed block size, removed placeholder text and created grid array

---
+++ script.js
@@ -0,0 +1,9 @@
+const game = (function(){
+    let gameBoard = {
+        field: [
+            ["o", "x", "o"], 
+            ["x", "x", "o"], 
+            ["o", "o", "x"]
+        ]
+    };
+})();
